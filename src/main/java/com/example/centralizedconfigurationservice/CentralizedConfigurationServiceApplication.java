e6fa6c0a1b1163b6d32bb693562cef796d0ca0da
Rajiv Srivastava <rajivmca2004@gmail.com>
2020-10-14 17:24:28 +0530

    Fixed cofngi file issue

--- src/main/java/com/example/centralizedconfigurationservice/CentralizedConfigurationServiceApplication.java
+++ src/main/java/com/example/centralizedconfigurationservice/CentralizedConfigurationServiceApplication.java
@@ -11,5 +11,4 @@ public class CentralizedConfigurationServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CentralizedConfigurationServiceApplication.class, args);
 	}
-
 }
